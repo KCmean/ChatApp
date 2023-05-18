4f5b1e2c0e31b76452acd3afca3bdc0df0772242
KCmean <kunalmanu2468@gmail.com>
2023-05-18 14:41:17 +0530

    feat: fixed avatars bug

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -11,7 +11,7 @@ import { Buffer } from "buffer";
 
 export default function SetAvatar() {
 
-  const api = 'https://api.multiavatar.com/45678985';
+  const api = 'https://api.multiavatar.com';
   const navigate = useNavigate();
   const [avatars, setAvatar] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -24,67 +24,105 @@ export default function SetAvatar() {
     theme: 'dark',
   };
   const setProfilePicture = async () => {};
-  useEffect( () => {
 
-    async function fetchAvatar() {
-    const data = [];
-    for (let i = 0; i < 4; i++) {
-      const image = await axios.get(
-        `${api}/${Math.round(Math.random() * 10000)}`
-      );
-      console.log(image);
-      const buffer =  Buffer.from(image.data);
-      data.push(buffer.toString("base64"));
-    }
-    setAvatar(data);
-    setLoading(false);
+  //   useEffect( () => {
 
-  }
-  fetchAvatar();  
-}, []);
-// useEffect(() => {
-//   async function fetchAvatar() {
-//     try {
-//       const data = [];
-//       const api = 'https://api.multiavatar.com/45678985';
+  //     async function fetchAvatar() {
+  //     const data = [];
+  //     const api = 'https://api.multiavatar.com';
+
+  //     for (let i = 0; i < 4; i++) {
+  //       const image = await axios.get(
+  //         `${api}/${Math.round(Math.random() * 10000)}`
+  //       );
+  //       console.log(image);
+  //       const buffer =  Buffer.from(image.data);
+  //       data.push(buffer.toString("base64"));
+  //     }
+  //     setAvatar(data);
+  //     setLoading(false);
+
+  //   }
+  //   fetchAvatar();  
+  // }, []);
+  // useEffect(() => {
+  //   async function fetchAvatar() {
+  //     try {
+  //       const data = [];
+  //       const api = 'https://api.multiavatar.com';
+  //       const api_key ='apikey=9ltuDFd82KEqSL'
 
 
-//       for (let i = 0; i < 4; i++) {
-//         const response = await fetch(`${api}/${Math.round(Math.random() * 1000)}`);
-//         if (!response.ok) {
-//           throw new Error('Error fetching avatars');
-//         }
-//         const image = await response.text();
+  //       for (let i = 0; i < 4; i++) {
+  //         const response = await fetch(`${api}/${Math.round(Math.random() * 1000000)}?${api_key}`);
+  //         console.log(response);
+  //         if (!response.ok) {
+  //           throw new Error('Error fetching avatars');
+  //         }
+  //         const image = await response.text();
 
-//         const buffer = Buffer.from(image, 'base64');
-//         data.push(buffer.toString('base64'));
-//       }
+  //         const buffer = Buffer.from(image, 'base64');
+  //         data.push(buffer.toString('base64'));
+  //       }
 
-//       setAvatar(data);
-//       setLoading(false);
-//     } catch (error) {
-//       console.error('Error fetching avatars:', error);
-//     }
-//   }
+  //       setAvatar(data);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       console.error('Error fetching avatars:', error);
+  //     }
+  //   }
 
-//   fetchAvatar();
-// }, []);
-  
-//   useEffect( () => {
+  //   fetchAvatar();
+  // }, []);
 
-//     async function fetchAvatar() {
-//     const data = [];
-//     for (let i = 0; i < 4; i++) {
-//       const image = await axios.get(
-//         `${api}/${Math.round(Math.random() * 1000)}`
-//       );
-//       const buffer =  Buffer.from(image.data);
-//       data.push(buffer.toString("base64"));
-//     }
-//     setAvatar(data);
-//     setLoading(false);
-// }}, []);
-  
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = [];
+  //       const api = 'https://api.multiavatar.com/78945';
+  //       const api_key = 'apikey=9ltuDFd82KEqSL'
+
+
+  //       for (let i = 0; i < 4; i++) {
+  //         const response = await fetch(`${api}/${Math.round(Math.random() * 1000000)}?${api_key}`);
+  //         if (!response.ok) {
+  //           throw new Error('Error fetching avatars');
+  //         }
+  //         // console.log(response);
+  //         const image = await response.text();
+
+  //         // console.log(image);
+
+  //         const buffer = Buffer.from(image, 'base64');
+  //         data.push(buffer.toString('base64'));
+  //         // console.log(data);
+  //       }
+
+  //       setAvatar(data);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       console.error('Error fetching avatars:', error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+  // console.log(avatars);
+
+    useEffect( () => {
+
+      async function fetchAvatar() {
+      const data = [];
+      for (let i = 0; i < 4; i++) {
+        const image = await axios.get(
+          `${api}/${Math.round(Math.random() * 1000)}?apikey=9ltuDFd82KEqSL`
+        );
+        const buffer =  Buffer.from(image.data);
+        data.push(buffer.toString("base64"));
+      }
+      setAvatar(data);
+      setLoading(false);
+  }fetchAvatar()}, []);
+
 
 
 
@@ -96,9 +134,9 @@ export default function SetAvatar() {
             Pick an avatar as your profile picture
           </h1>
           <div className='avatars'>
-            {avatars.map((avatar, index) =>{
+            {avatars.map((avatar, index) => {
               return (
-                <div className={`avatar ${selectedAvatar === index ? "selected" : "" }`}>
+                <div className={`avatar ${selectedAvatar === index ? "selected" : ""}`}>
                   <img
                     src={`data:image/svg+xml;base64,${avatar}`}
                     alt="avatar"
@@ -110,6 +148,9 @@ export default function SetAvatar() {
             })}
           </div>
         </div>
+        <button className='submit-btn' onClick={setProfilePicture}>
+          Set As Profile Picture
+        </button>
       </Container>
       <ToastContainer />
     </>
@@ -117,4 +158,61 @@ export default function SetAvatar() {
 }
 
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  background-color: #131324;
+  height: 100vh;
+  width: 100vw;
+  .loader{
+    max-inline-size: 100%; 
+  }
+  .title-container{
+    h1{
+      color: #fff;
+      padding-bottom: 2rem;
+    }
+  }
+  .avatars{
+    display: flex;
+    gap: 2rem;
+    .avatar{
+      border: 0.4rem solid transparent;
+      padding: 0.5rem;
+      /* padding-top: 1rem; */
+      border-radius: 5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: 0.3s ease-in-out;
+      cursor: pointer;
+      img{
+        height: 6rem;
+      } 
+    }
+    .selected{
+      border: 0.4rem solid #4e0eff;
+    }
+  }
+  .submit-btn{
+      background-color: #997af0;
+      color: #fff;
+      padding: 1rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      &:hover{
+        background-color: #4e0eff;
+        transition: 0.5s ease-in-out;
+      }
+
+      
+    }
+
+`;
